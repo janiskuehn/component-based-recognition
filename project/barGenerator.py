ef6f74664879da9636ed8f9a850c9aad33de21af
Janis Kühn <jk@stud.uni-frankfurt.de>
2017-09-04 11:23:53 +0200

    fixes, added as_neuralstate, to_file, generate set of distinct patterns

--- project/barGenerator.py
+++ project/barGenerator.py
@@ -1,5 +1,7 @@
 import numpy as np
 from PIL import Image as img
+from neural import NeuralState
+import os
 
 
 class BarTest:
@@ -10,7 +12,8 @@ class BarTest:
     bars_active: np.ndarray = None
     
     def __init__(self, bps: int, ppb: int = 1, density: float = DEFAULT_DENSITY,
-                 density_x: float = DEFAULT_DENSITY, density_y: float = DEFAULT_DENSITY):
+                 density_x: float = DEFAULT_DENSITY, density_y: float = DEFAULT_DENSITY,
+                 from_file: str = None):
         """
         Generate a random Bars-Test with probabilities for a bar to be active.
         :param bps: "Bars per side" -> How many bars in each dimension.
@@ -19,22 +22,39 @@ class BarTest:
         :param density_x: Probability for a vertical bar to be active.
         :param density_y: Probability for a horizontal bar to be active.
         """
-        if density != self.DEFAULT_DENSITY:
-            density_x = density
-            density_y = density
+        if from_file:
+            """
+            f = open(from_file, "r", encoding="ASCII")
+            lines = f.readlines()
+            f.close()
             
-        self.ppb = ppb
-        self.bps = bps
-        self.size = ppb * bps
-        c_x = np.random.randint(0, bps, (int(density_x * bps)), dtype=int)
-        c_y = np.random.randint(0, bps, (int(density_y * bps)), dtype=int)
-        c_x += bps
-        b = np.zeros((bps * 2), dtype=int)
-        b[c_x] = 1
-        b[c_y] = 1
-        self. bars_active = b.reshape((2, bps))
+            self.ppb = lines[-1]
+            self.bps = lines[-2]
+            lines = lines[:-2]
+
+            f = open(from_file, "2", encoding="ASCII")
+            f.writelines(lines)
+            f.close()
+            """
+            self.bars_active = np.load(from_file)
+            
+        else:
+            if density != self.DEFAULT_DENSITY:
+                density_x = density
+                density_y = density
+                
+            self.ppb = ppb
+            self.bps = bps
+            self.size = ppb * bps
+            c_x = np.random.randint(0, bps, (int(density_x * bps)), dtype=int)
+            c_y = np.random.randint(0, bps, (int(density_y * bps)), dtype=int)
+            c_x += bps
+            b = np.zeros((bps * 2), dtype=int)
+            b[c_x] = 1
+            b[c_y] = 1
+            self.bars_active = b.reshape((2, bps))
         
-    def as_array(self) -> np.ndarray:
+    def as_matrix(self) -> np.ndarray:
         """
         Create an Numpy array for this bar test
         :return: 2D Numpy array, 1 -> active, 0 -> inactive
@@ -47,6 +67,15 @@ class BarTest:
                     ret[i][j] = 1
                     
         return ret
+    
+    def as_neuralstate(self) -> NeuralState:
+        """
+        Create a neural state with the generated vector as neural vector
+        :return: NeuralState
+        """
+        v = self.as_matrix().flatten()
+        n = NeuralState(self.size, self.size, False, -1, initial_vector=v)
+        return n
     
     def as_image(self) -> img:
         """
@@ -63,3 +92,39 @@ class BarTest:
                             (255, 255, 255)
                             )
         return im
+
+    def to_file(self, fn: str):
+        """
+        Save bar test to file.
+        :param fn: File path to save to
+        :return: -
+        """
+        np.save(fn, self.bars_active)
+        """
+        f = open(fn, "a", encoding="ASCII")
+        l = [str(self.bps), str(self.ppb)]
+        f.writelines(l)
+        f.close()
+        """
+
+
+def generate_all_distinct_lines(bps: int, ppb: int, to_path: str = None) -> list:
+    if to_path:
+        try:
+            os.mkdir(to_path)
+        except OSError:
+            pass  # Folder exists
+    b = BarTest(bps, ppb)
+    shp = b.bars_active.shape
+    b.bars_active = np.zeros(shp)
+    res = []
+    for z in [0, 1]:
+        for xy in range(bps):
+            zn = "x" if z == 1 else "y"
+            b.bars_active[z][xy] = 1
+            if to_path:
+                b.to_file(to_path+"/bars_" + zn + str(xy) + ".npy")
+                b.as_image().save(to_path+"/bars_" + zn + str(xy) + ".png", "PNG")
+            res.append(b.bars_active.copy())
+            b.bars_active[z][xy] = 0
+    return res
