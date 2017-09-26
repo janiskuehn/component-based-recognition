0c6e61a72893bf6bb8d379135c219b68c86e0de3
Janis Kühn <jk@stud.uni-frankfurt.de>
2017-09-26 07:40:27 +0200

    splitting of matrix_indices to be used in multiprocessing

--- project/utils.py
+++ project/utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+from datetime import datetime as dati
+from math import ceil
 
 
 def print_matrix(matrix: np.ndarray):
@@ -21,3 +23,34 @@ def metadata(size: int, r: float, dt: float, steps: int, a: float) -> str:
 
 def fts(inp) -> str:  # Float to String
     return ('%.15f' % inp).rstrip('0').rstrip('.')
+
+
+def index_clustering_by_count(m: np.ndarray, bc: int, min_bs: int) -> list:
+    """
+    Returns index-packages of size bs_i * bs_j
+    :param m: matrix
+    :param bc: blockcount
+    :param min_bs: minimal blocksize in elements
+    :return: list of tupels
+    """
+    ret = []
+    
+    l_i = m.shape[0]
+    l_j = m.shape[1]
+    bs_i = max(ceil(l_i / bc), min_bs // l_j)
+    bc = ceil(l_i / bs_i)
+    
+    splits_i = []
+    for i in range(bc):
+        splits_i.append((i * bs_i, min(l_i, (i + 1) * bs_i)))
+    
+    splits_j = (0, l_j)
+    
+    for x in splits_i:
+        ret.append((x, splits_j))
+        
+    return ret
+
+
+def log_print(s: str):
+    print(dati.now().strftime('%H:%M:%S,%f') + ' ' + s)
